cf32e282eb25a81c0722ea0421cce68e08c0fc55
nilekazr <nile.ausar1@gmail>
2020-10-07 15:40:26 -0400

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
-// objects for each player
+// object for each player
 
-const players = {
-
-    name1: "",
-    name2: "",
-    hasWon: false,
-    myTurn:  false,
-    gridToken: 1,
-    player: 1
-};
+const players = [
+    { name: "bob", hasWon: false, },
+    { name: "jeff", hasWon: false, },
+]
+let playerToken = 1;
 // function to create a grid
-
     gridArray = [
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
@@ -20,37 +15,40 @@ const players = {
         [0,0,0,0,0,0,0],
     ];
 // function to place checkers
-function dropToken(col) {
-    for (i = 0; gridArray[i] < gridArray.length; i ++){
-        if (gridArray[i] == 0){
-            const innerArray = gridArray[i].length;
-            for (j = 0; innerArray[j] < innerArray.length; j ++ ) {
-                innerArray[col] = players.gridToken;
-            }
+console.log('hello');
+function dropToken(row,col) {
+    let gridSpot = gridArray[0][row][col];
+        while (gridSpot !== 0){
+            gridSpot = gridArray[0][row-1][col];
         }
+    if(gridSpot == 0){
+        gridSpot = playerToken;
+        colorswap();
+        nextTurn();
     }
-    nextTurn();
 }
-dropToken(4);
-console.log(gridArray);
+
+    //nextTurn();
 
  //function to change/count turns
-
-function nextTurn(){
-    if (players.player == 1){
-        players.player ++;
-        players.gridToken ++;
-        return;
-    }
-    if (players.player == 2){
-        players.player --;
-        players.gridToken --;
-        return;
-    }
+function colorswap(){
+    if (gridArray[0][row][col]==0) { 
+        document.getElementById("cell"+row+col).style.backgroundColor="#FFFFFF";
+} else if (gridArray[row][col]==1) {
+        document.getElementById("cell"+row+col).style.backgroundColor="#FFFF00";
+} else if (gridArray[row][col]==2) { 
+        document.getElementById("cell"+row+col).style.backgroundColor="#FF0000";
 }
-
-//console.log(players);
-
+}
+function nextTurn(){
+    if (playerToken == 1){
+        playerToken++;
+        return;
+    }
+    if (playerToken == 2){
+        playerToken--;
+        return;
+    }
+        
+}
 // function to check for a win
-
-
